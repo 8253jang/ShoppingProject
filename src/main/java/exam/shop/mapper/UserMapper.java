b5d4d9f37daa93ea8dece46b27e2d7393e1f524e
iMAC00 <iMAC00@iMAC00-PC>
2014-11-17 14:53:16 +0900

    df

---
+++ src/main/java/exam/shop/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package exam.shop.mapper;
+
+import exam.shop.dto.*;
+
+public interface UserMapper {
+	public abstract void registerUser(User user);
+	public abstract User getUserByUserId(User user);
+}
